1c9c75e19d605f363fbc6de43591742fd7014acf
Cassandra Santillano <casantillano@csu.fullerton.edu>
2026-02-23 13:54:49 -0800

    initial commit

---
+++ src/main/java/com/example/Web_Backend_Engineering/WebBackendEngineeringApplication.java
@@ -0,0 +1,13 @@
+package com.example.Web_Backend_Engineering;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebBackendEngineeringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebBackendEngineeringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/Web_Backend_Engineering/WebBackendEngineeringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Web_Backend_Engineering;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebBackendEngineeringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
